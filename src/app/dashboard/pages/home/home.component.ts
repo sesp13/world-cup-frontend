48574051536f92b907b219706c234776f68dbbcb
sesp13 <sespinosarte99@gmail.com>
2022-08-28 01:31:48 -0500

    add provided and pending menus to dashboard

--- src/app/dashboard/pages/home/home.component.ts
+++ src/app/dashboard/pages/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PagingParams } from 'src/app/interfaces/paging-params';
 import { GetStickersByUserStatusResponse } from 'src/app/interfaces/responses/sticker-responses';
 import { ISticker } from 'src/app/interfaces/sticker';
 import { IUser } from 'src/app/interfaces/user.interface';
@@ -18,49 +19,99 @@ export class HomeComponent implements OnInit {
   skipPendingStickers: number = 0;
   limitPendingStickers: number = 10;
 
+  providedStickers: ISticker[] = [];
+  totalProvidedStickers: number = 0;
+  skipProvidedStickers: number = 0;
+  limitProvidedStickers: number = 4;
+
+  repeatedStickers: ISticker[] = [];
+  totalRepeatedStickers: number = 0;
+  skipRepeatedStickers: number = 0;
+  limitRepeatedStickers: number = 6;
+
   constructor(
     private authService: AuthService,
     private stickerService: StickerService
   ) {}
 
   ngOnInit(): void {
-    this.getPendingStickers();
+    this.getStickers('PENDING');
+    this.getStickers('PROVIDED');
+    this.getStickers('REPEATED');
   }
 
-  getPendingStickers(): void {
-    this.stickerService
-      .getStickersByUserStatus('PENDING', {
-        paged: true,
-        skip: this.skipPendingStickers,
-        limit: this.limitPendingStickers,
-      })
-      .subscribe({
-        next: (response: GetStickersByUserStatusResponse) => {
-          this.pendingStickers =
-            response.stickers !== undefined
-              ? [...this.pendingStickers, ...response.stickers]
-              : [];
-          this.totalPendingStickers = response.totalStickers ?? 0;
-        },
-        error: (error) => {
-          console.log(error);
-          alert('Error on pending stickers');
-        },
-      });
+  getStickers(type: string): void {
+    const params: PagingParams | undefined = this.parsePagingParams(type);
+    if (!params) return;
+
+    this.stickerService.getStickersByUserStatus(type, params).subscribe({
+      next: (response: GetStickersByUserStatusResponse) => {
+        const newStickers = response.stickers ?? [];
+        const totalStickers = response.totalStickers ?? 0;
+        switch (type) {
+          case 'PENDING':
+            this.pendingStickers = [...this.pendingStickers, ...newStickers];
+            this.totalPendingStickers = totalStickers;
+            break;
+          case 'PROVIDED':
+            this.providedStickers = [...this.providedStickers, ...newStickers];
+            this.totalProvidedStickers = totalStickers
+            break;
+          case 'REPEATED':
+            this.repeatedStickers = [...this.repeatedStickers, ...newStickers];
+            this.totalRepeatedStickers = totalStickers;
+            break;
+          default:
+            return;
+        }
+      },
+      error: (error) => {
+        console.log(error);
+        alert('Error on pending stickers');
+      },
+    });
+  }
+
+  parsePagingParams(type: string): PagingParams | undefined {
+    let skip: number, limit: number;
+    switch (type) {
+      case 'PENDING':
+        skip = this.skipPendingStickers;
+        limit = this.limitPendingStickers;
+        break;
+      case 'PROVIDED':
+        skip = this.skipProvidedStickers;
+        limit = this.limitPendingStickers;
+        break;
+      case 'REPEATED':
+        skip = this.skipRepeatedStickers;
+        limit = this.limitRepeatedStickers;
+        break;
+      default:
+        return undefined;
+    }
+    return { paged: true, skip, limit };
   }
 
   seeMoreStickers(type: string): void {
     switch (type) {
-      case 'pending': {
-        // Update pagination
+      case 'PENDING': {
         this.skipPendingStickers += this.limitPendingStickers;
-        this.getPendingStickers();
+        this.getStickers('PENDING');
+        break;
+      }
+      case 'PROVIDED': {
+        this.skipProvidedStickers += this.limitProvidedStickers;
+        this.getStickers('PROVIDED');
+        break;
+      }
+      case 'REPEATED': {
+        this.skipProvidedStickers += this.limitProvidedStickers;
+        this.getStickers('REPEATED');
         break;
       }
     }
   }
-
-
 
   logout(): void {
     this.authService.logout();
